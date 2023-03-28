31cfb79a9f2529bbb1bd6910ae8be9398ca7da25
Carenjot <carenjot.aurelien@hotmail.fr>
2023-03-28 11:39:28 +0200

    Ajout de pokémon dans la liste et de deux boutons pour les parcourir

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,47 @@ import './App.css'
 import PokemonCard from './components/PokemonCard'
 const pokemonList = [
   {
-    name:"bulbasaur",
-    imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-const pokemon = pokemonList[0];
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
-
+  const [index, setIndex] = useState(0)
+  let pokemon = pokemonList[index];
+  const previousPokemon = () => {
+    if(index>0){
+      setIndex( index - 1);
+    }
+  }
+  const nextPokemon = () => {
+    if(index<pokemonList.length-1){
+      setIndex( index + 1);
+    }
+  }
 
   return (
     <div className="App">
       <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc}/>
+      <button onClick={previousPokemon}>Précédent</button>
+      <button onClick={nextPokemon}>Suivant</button>
     </div>
     
   )
