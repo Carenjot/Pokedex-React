4cd9eab72ecd08f1f6c75450fc886eabb7e0af51
Carenjot <carenjot.aurelien@hotmail.fr>
2023-03-28 10:20:34 +0200

    Première utilisation des props

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
+const pokemonList = [
+  {
+    name:"bulbasaur",
+    imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+const pokemon = pokemonList[0];
 
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
+
   return (
     <div className="App">
-      <PokemonCard/>
+      <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc}/>
     </div>
     
   )
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,23 +1,13 @@
-const pokemonList = [
-    {
-      name:"bulbasaur",
-      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard(props) {
+  console.log(props);
     return (
       <figure className="card">
-        {pokemon.imgSrc === undefined ? <p>???</p> : <img
-    src={pokemon.imgSrc}
-    alt={pokemon.name}
+        {props.imgSrc === undefined ? <p>???</p> : <img
+    src={props.imgSrc}
+    alt={props.name}
     className="card-img"
   />}
-        <figcaption>{pokemon.name}</figcaption>
+        <figcaption>{props.name}</figcaption>
       </figure>
     );
   }
